fa1387c5d96ef817a42a046b6ebf11be61a77187
John Ramos <jramos5883@gmail.com>
2023-06-07 16:56:30 -0700

    Finishing up nav bar set up.

--- myspace-clone-app/app/layout.tsx
+++ myspace-clone-app/app/layout.tsx
@@ -1,21 +1,26 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import NavMenu from "./navmenu";
 
-const inter = Inter({ subsets: ['latin'] })
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Myspace Clone App',
-  description: 'Building Myspace with React and Next 13!',
-}
+  title: "Myspace Clone App",
+  description: "Building Myspace with React and Next 13!",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NavMenu />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
