f0370b8d12eb70f238d8d87c92936c0573375e2a
John Ramos <jramos5883@gmail.com>
2023-06-13 12:09:24 -0700

    reverting changes

---
+++ myspace-clone-app/app/api/follow/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { getServerSession } from 'next-auth';
+import { prisma } from '@/lib/prisma';
+import { authOptions } from '../auth/[...nextauth]/route';
+
+export async function Post(req: Request) {
+
+}
+
+export async function Delete(req:NextRequest) {
+    
+}
